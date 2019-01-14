66e53ab9fde1a710b2fd1cd6c4e28320c8763193
Kerri Shotts <shotts@adobe.com>
2019-01-13 21:01:53 -0500

    Updates

---
+++ precache-manifest.34376c3ebf30268b18d0b527dd5746ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-starter/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "18eedbb68b8803eb68d1",
+    "url": "/react-starter/static/js/runtime~main.18eedbb6.js"
+  },
+  {
+    "revision": "23d3bef297cd064d5d73",
+    "url": "/react-starter/static/js/main.23d3bef2.chunk.js"
+  },
+  {
+    "revision": "d2c9bbc08969215e99b9",
+    "url": "/react-starter/static/js/1.d2c9bbc0.chunk.js"
+  },
+  {
+    "revision": "23d3bef297cd064d5d73",
+    "url": "/react-starter/static/css/main.a9be1492.chunk.css"
+  },
+  {
+    "revision": "d2c9bbc08969215e99b9",
+    "url": "/react-starter/static/css/1.f859a83f.chunk.css"
+  },
+  {
+    "revision": "0c26b9c32d108ab8c38da4d791b8f940",
+    "url": "/react-starter/index.html"
+  }
+];
